9ec1183c902579568c73561aa818d7f7c872c1e3
Juan David Adarve <juanda0718@gmail.com>
2020-05-19 21:40:51 -0500

    Added build script for generating Doxygen.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+git clone -b master --depth 1 https://github.com/jadarve/lluvia.git
+
+# Generate Doxygen documentation
+cd lluvia
+mkdir -p build/doc
+
+doxygen Doxyfile
+
+# back to root
+cd ..
+
+# move to the static folder so that Hugo can pack it with the generated site
+mkdir -p static/api
+mv lluvia/build/doc/html static/api/cpp
+
+# generate site
+hugo --gc --minify
