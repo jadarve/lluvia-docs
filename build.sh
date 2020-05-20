d57740c509f9cab9e2130966909a85e3453b9122
Juan David Adarve <juanda0718@gmail.com>
2020-05-19 21:56:37 -0500

    Removed all fancy cloning and building Doxygen doc stuff.

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-# install dependencies
-apt update
-apt install -y doxygen
+# # install dependencies
+# sudo apt update
+# sudo apt install -y doxygen
 
-git clone -b master --depth 1 https://github.com/jadarve/lluvia.git
+# git clone -b master --depth 1 https://github.com/jadarve/lluvia.git
 
-# Generate Doxygen documentation
-cd lluvia
-mkdir -p build/doc
+# # Generate Doxygen documentation
+# cd lluvia
+# mkdir -p build/doc
 
-doxygen Doxyfile
+# doxygen Doxyfile
 
-# back to root
-cd ..
+# # back to root
+# cd ..
 
-# move to the static folder so that Hugo can pack it with the generated site
-mkdir -p static/api
-mv lluvia/build/doc/html static/api/cpp
+# # move to the static folder so that Hugo can pack it with the generated site
+# mkdir -p static/api
+# mv lluvia/build/doc/html static/api/cpp
 
 # generate site
 hugo --gc --minify
