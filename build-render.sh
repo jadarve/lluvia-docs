a15370cc1b43735d8519108a6ca9bc95915390a2
Juan David Adarve <juanda0718@gmail.com>
2021-06-09 23:15:07 -0500

    It works!

--- build-render.sh
+++ build-render.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
-# npm config set prefix="${HOME}/npm"
-
-# npm install -g postcss
-# npm install -g postcss-cli
-# npm install -g autoprefixer
-
-# export PATH=~/node_modules/.bin:$PATH
-
 npm install
-
-pwd
-ls
 
 git submodule update --init --recursive --depth 1
 hugo --gc --minify
