31b04dd48c10e89ab2f40bc93284e4f92675089d
Juan David Adarve <juanda0718@gmail.com>
2021-06-09 23:12:58 -0500

    Added package.json

--- build-render.sh
+++ build-render.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-npm config set prefix="${HOME}/npm"
+# npm config set prefix="${HOME}/npm"
 
-npm install -g postcss
-npm install -g postcss-cli
-npm install -g autoprefixer
+# npm install -g postcss
+# npm install -g postcss-cli
+# npm install -g autoprefixer
 
-export PATH=~/node_modules/.bin:$PATH
+# export PATH=~/node_modules/.bin:$PATH
+
+npm install
+
+pwd
+ls
 
 git submodule update --init --recursive --depth 1
 hugo --gc --minify
